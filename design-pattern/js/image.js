9d6cb55585b131b98373cd2b6e7ba2a06e05b94d
oskarmast <oskarmast9436@gmail.com>
2020-12-16 22:35:35 +0800

    js代理模式预加载图片

--- design-pattern/js/image.js
+++ design-pattern/js/image.js
@@ -10,4 +10,18 @@ var create_image = (function(){
 
 let url = "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1405513753,2858193846&fm=26&gp=0.jpg";
 
-create_image.setSrc(url);
+var proxyImage = (function(){
+	var img = new Image();
+	img.onload = function(){
+		create_image.setSrc(this.src);
+	}
+	return {
+		setSrc:function(src) {
+			create_image.setSrc('/js/images/timg.gif');
+			img.src = src;
+		}
+	}
+})()
+proxyImage.setSrc(url);
+
+
