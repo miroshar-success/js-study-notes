4c8cdbcf50d5f6955d8e31577021b061b37bb4e3
oskarmast <oskarmast9436@gmail.com>
2021-06-06 11:38:29 +0800

    redux-demo

--- 深入浅出React和Redux/Redux-Examples/js/PostFeed.jsx
+++ 深入浅出React和Redux/Redux-Examples/js/PostFeed.jsx
@@ -7,8 +7,21 @@ let initial_posts = [
 ]
 
 function posts (state = initial_posts,action){
+  switch(action.type){
+    case 'post/ADD_POST':
+      let id = state.length > 0 ? state[state.length-1]['id'] + 1 : 1;
+      return [...state,{id,...action.payload}]
+  }
   return state
 }
+
+const addPost = (payload) => (
+  {
+    type:"post/ADD_POST",
+    payload
+  }
+)
+
 
 const reducer = combineReducers({
   posts
@@ -23,6 +36,7 @@ class PostList extends React.Component {
     super(props);
   }
   render(){
+    console.log(this.props.posts);
     return (
       <div>
         <h1>Posts</h1>
@@ -45,9 +59,55 @@ const mapStateToProps = state => {
 }
 PostList = connect(mapStateToProps,null)(PostList);
 
+function AddPostForm(props){
+  const [title,setPostTitle] = React.useState("");
+  const [content,setContent] = React.useState("")
+  function handleSubmit(){
+    if(!title){
+      window.alert("标题不能为空");
+      return;
+    }
+    if(!content){
+      window.alert("内容不能为空")
+      return;
+    }
+    props.addPost({
+      title,
+      content
+    })
+    setPostTitle("");
+    setContent("");
+  }
+  function onTitleChanged(event){
+    let title = event.target.value.trim();
+    setPostTitle(title);
+  }
+  function onContentChanged(event){
+    let content = event.target.value.trim();
+    setContent(content);
+  }
+  return (
+    <section>
+      <div className="block">
+        <label htmlFor="postTitle">Post Title: </label>
+        <input type="text" name="postTitle" value={title} onChange={onTitleChanged}/>
+      </div>
+      <div className="block">
+        <label htmlFor="postContent">Content:</label>
+        <textarea value={content} onChange={onContentChanged}></textarea>
+      </div>
+      <input type="submit" value="提交" className="submit-button" onClick={handleSubmit}/>
+    </section>
+  )
+}
+AddPostForm = connect(null,{addPost})(AddPostForm);
+
 ReactDOM.render(
   <Provider store={store}>
-    <PostList/>
+    <React.Fragment>
+      <PostList/>
+      <AddPostForm/>
+    </React.Fragment>
   </Provider>,
   document.getElementById("post-feed")
 )
