cb2e5c911f8736b5d9342277b4c9926e8e8fb885
oskarmast <oskarmast9436@gmail.com>
2021-07-28 22:35:54 +0800

    异步编程-promise

---
+++ 异步编程/setTimeout.js
@@ -0,0 +1,10 @@
+setTimeout(function timer1() {
+  console.log('timer1 invoke')
+},1800)
+
+setTimeout(function timer2(){
+  console.log('timer2 invoke');
+  setTimeout(function inner(){
+    console.log('inner invoke')
+  },1000)
+},700)
