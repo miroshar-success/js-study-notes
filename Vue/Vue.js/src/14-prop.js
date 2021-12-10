4d50a379f4927e0d17e19aab8159ef063cf9251f
oskarmast <oskarmast9436@gmail.com>
2021-12-10 22:34:59 +0800

    watch-computed

--- Vue/Vue.js/src/14-prop.js
+++ Vue/Vue.js/src/14-prop.js
@@ -24,3 +24,80 @@ const prop_instance = new Vue({
 })
 
 console.log(prop_instance)
+
+
+const Comp = Vue.extend({
+  props:{
+    msg:{
+      type:String,
+      default:''
+    }
+  },
+  template:`<div>{{msg}}</div>`
+})
+
+const p_instance = new Comp({
+  el:"#prop-el",
+  propsData:{
+    msg:'hello world123'
+  }
+})
+console.log('p_instance:',p_instance)
+
+
+
+
+const Child_Component = Vue.component('child-component',{
+  props:{
+    message:{
+      type:String,
+      default:''
+    },
+    msg:{
+      type:String
+    }
+  },
+  data(){
+    return {}
+  },
+  template:`<div>hello, 我是子组件 {{message}}</div>`,
+  methods:{
+    hello(){
+      console.log('hello')
+    },
+    _hello(){
+      console.log('hello')
+    },
+    $hello(){
+      console.log('hello')
+    },
+  }
+})
+
+const c_instance = new Vue({
+  el:'#prop-element',
+  data(){
+    return {
+      message:'hello,我来自父组件',
+      msg:''
+    }
+  },
+  created(){
+    console.log('created--------', this.message)
+  },
+  // methods:{
+  //   message(){
+  //     console.log('我是一个方法')
+  //   }
+  // }
+})
+
+console.log('c:' ,c_instance)
+
+
+function Player(name){
+  this.name = name;
+}
+Player.prototype.message = 'hello';
+const kyrie = new Player('kyrie')
+console.log(kyrie, kyrie.name, kyrie.message)
---
+++ Vue/Vue.js/src/16-watch.js
@@ -0,0 +1,64 @@
+const watch_instance = new Vue({
+  el:'#watch-element',
+  data() {
+    return {
+      count:0,
+      player:{
+        age:30
+      }
+    }
+  },
+  watch: {
+/*     count:function(v,o) {
+      console.log('count:', v,o)
+    } */
+
+    //  给count的属性传递一个数组
+/*     count:[function w1(w,v){
+      console.log('w1',w, v)
+    }, function w2(w, v){
+      console.log('w2',w, v)
+    }, function w3(w, v){
+      console.log('w3', w, v)
+    }] */
+    // count 为一个对象
+    count:{ // 立即执行
+      handler:function(v){
+        console.log('v', v)
+      },
+      immediate:true
+    },
+    // ------------------ 深度监听 ---------------
+    player:{
+      handler:function(v){
+        console.log('player',v)
+      },
+      deep:true
+    },
+    // ------------------- 监听对象的属性 ---------------
+    'player.age':function(){
+      console.log('player-age-changed')
+    }
+  },
+  methods:{
+    click() {
+      this.count += 1;
+      this.player.age += 1
+    }
+  }
+})
+
+
+const watch_object = {
+  count(){
+  },
+  age:{
+    handler:function(){
+    }
+  }
+}
+
+for(let key in watch_object){
+  const handler = watch_object[key] // function
+  console.log(key, typeof handler)  // object
+}
