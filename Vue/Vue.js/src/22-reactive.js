e1e6566303452095529b2ce2d613da4b439cd013
oskarmast <oskarmast9436@gmail.com>
2021-12-26 21:43:10 +0800

    vue原理

--- Vue/Vue.js/src/22-reactive.js
+++ Vue/Vue.js/src/22-reactive.js
@@ -11,6 +11,8 @@ function reactive(target) {
   const handler = {
     get:function(target, prop, receiver){
       const result = Reflect.get(target, prop, receiver)
+      //  收集依赖
+      track(target, prop)
       return convert(result)
     },
     set:function(target, prop, value, receiver){
@@ -18,13 +20,14 @@ function reactive(target) {
       let result = true;
       if(oldValue !== value){
         result = Reflect.set(target, prop, value, receiver)
+        trigger(target, prop)
       }
       return result
     },
     deleteProperty:function(target, key){
       const result = Reflect.deleteProperty(target, key)
       if(hasOwn(target,key) && result){
-        console.log('delete', key)
+        trigger(target, prop)
       }
       return result
     }
@@ -32,4 +35,101 @@ function reactive(target) {
   return new Proxy(target, handler)
 }
 
-export default reactive
+
+// ----- 目标对象 ---- 属性 ----- 箭头函数 ------
+let activeEffect = null
+function effect(callback) {
+  activeEffect = callback
+  callback()
+  activeEffect = null
+}
+// ---------收集依赖---------
+const targetMap = new WeakMap()
+function track(target, key) {
+  if(!activeEffect) return;
+  let depsMap = targetMap.get(target)
+  if(!depsMap){
+    targetMap.set(target, (depsMap = new Map()))
+  }
+  let dep = depsMap.get(key)
+  if(!dep){
+    depsMap.set(key, (dep = new Set()))
+  }
+  dep.add(activeEffect)
+}
+
+
+function trigger(target,key){
+  const depsMap = targetMap.get(target)
+  if(!depsMap) return;
+  const deps = depsMap.get(key)
+  if(deps){
+    deps.forEach(effect => {
+      effect()
+    })
+  }
+}
+
+
+// ----------- ref ------------
+// __v_isRef 标识是一个ref对象
+function ref(raw) {
+  if(isPlainObject(raw) && raw.__v_isRef) return raw
+  let value = convert(raw)  // 如果是对象,转换为一个响应式对象,否则返回值本身
+  const r = {
+    __v_isRef: true,
+    get value() {
+      track(r, 'value')
+      return value
+    },
+    set value(newValue){
+      if(value !== newValue){
+        raw = newValue;
+        value = convert(raw)
+        trigger(r, 'value')
+      }
+    }
+  }
+  return r
+}
+
+
+// ------------- toRefs --------------
+// 将解构的对象转换为响应式
+function toRefs(proxy) {
+  const ret = {}
+  for(const key in proxy){
+    ret[key] = toProxyRef(proxy, key)
+  }
+  return ret
+}
+
+function toProxyRef(proxy, key){
+  const r = {
+    __v_isRef: true,
+    get value() {
+      return proxy[key]
+    },
+    set value(newValue){
+      proxy[key] = newValue;
+    }
+  }
+  return r;
+}
+
+
+// ---------------- computed ----------------
+function computed(getter){
+  const result = ref()
+  effect(() => {
+    result.value = getter()
+  })
+  return result
+}
+
+export {
+  reactive,
+  effect,
+  ref,
+  toRefs
+}
