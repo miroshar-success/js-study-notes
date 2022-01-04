ee0ddcbff4b2d6b1ef42bab7906c2d1e309141fb
oskarmast <oskarmast9436@gmail.com>
2022-01-04 23:29:41 +0800

    promise

--- 异步编程/promise.js
+++ 异步编程/promise.js
@@ -1,149 +1,75 @@
-// // return promise
-// function timeout(ms) {
-//   return new Promise(resolve => {
-//     setTimeout(resolve,ms,'done')
-//   })
-// }
+// ----------------- Promise构造函数 基本使用 --------------------
+function timeout(ms) {
+  return new Promise((resolve,reject) => {
+    setTimeout(resolve, ms, 'done')
+  })
+}
 
-// timeout(2000).then(value => {
-//   console.log('value:',value)
-// })
-
-
-// // promise执行顺序
-// let promise = new Promise(resolve => {
-//   console.log('Promise')
-//   resolve()
-// })
-// promise.then(() => {
-//   console.log('resolved')
-// })
-// console.log('Hi')
-// /*
-// Promise
-// Hi 
-// resolved
-// */ 
-
-
-// // promise的状态改变了就不会改变
-// const p1 = new Promise((resolve,reject) => {
-//   resolve('hello');
-//   reject(new Error('error,promise rejected'))
-// })
-// p1.then(value => {
-//   console.log('value:',value)
-// }).catch(err => {
-//   console.log('err:',err)
-// })
-
-// const p2 = new Promise((resolve,reject) => {
-//   reject('err,promise rejected')
-//   resolve('hello')
-// })
-// p2.then(value => {
-//   console.log('value:',value)
-// }).catch(err => {
-//   console.log('err:',err)
-// });
-
-
-// // 懒加载图片
-// // function loadImageAsync(url) {
-// //   return new Promise((resolve,reject) => {
-// //     const image = new Image();
-// //     image.onload = function(){
-// //       resolve(image)
-// //     }
-// //     image.onerror = function(){
-// //       reject(new Error('Could not load image at' + url))
-// //     }
-// //     image.src = url;
-// //   })
-// // }
-// // loadImageAsync('https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fpic2.zhimg.com%2F50%2Fv2-193cbb243dc14d3a016caaa54ba02837_hd.jpg&refer=http%3A%2F%2Fpic2.zhimg.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1630162244&t=a1165a8ef8e3f5ddf04b5f6f603088cf')
-// // .then(img => {
-// //   console.log(img)
-// // })
-
-
-// console.log(new Date())
-// // promise封装ajax
-// function ajax(url){
-//   const xhr = new XMLHttpRequest()
-//   xhr.open('GET', url, true)
-//   xhr.responseType = 'json'
-//   xhr.send(null)
-//   return new Promise(resolve => {
-//     xhr.onload = function() {
-//       resolve(xhr.response)
-//     }
-//   })
-// }
-
-// // ajax('http://apis.juhe.cn/simpleWeather/query?city=hangzhou')
-// // .then(res => {
-// //   console.log('res:', res)
-// // })
-
-
-// const promise1 = new Promise(resolve => {
-//   resolve('hello')
-// })
-// const promise2 = new Promise(resolve => {
-//   resolve('world')
-// })
-// Promise.all([promise1,promise2]).then(result => {
-//   console.log(result);
-// })
-
-
-const promise3 = new Promise((resolve) => {
-  setTimeout(() => {
-    resolve('hello world')
-  },3000)
-})
-const promise4 = new Promise((resolve,reject) => {
-  setTimeout(() => {
-    reject('请求超时')
-  },4000)
-})
-Promise.race([promise3,promise4]).then((v) => {
-  console.log(v)
-})
-.catch((err) => {
+timeout(1000).then(value => {
+  console.log('value', value) // done
+},function(err) {
   console.log(err)
 })
 
-Promise.allSettled([promise3,promise4]).then(v => {
-  console.log(v)
-  // [
-  //   { status: 'fulfilled', value: 'hello world' },
-  //   { status: 'rejected', reason: '请求超时' }
-  // ]
+
+// --------------------- Promise新建后就会立即执行 -------------------
+const promise = new Promise((resolve,reject) => {
+  console.log('promise')
+  resolve()
+})
+promise.then(function(value){
+  console.log(value)  // undefined
 })
 
-const promise_prototype = new Promise((resolve,reject)=>{
 
+// ---------------- Promise 返回一个promise ------------------
+const p1 = new Promise((resolve,reject) => {
+  setTimeout(() => {
+    reject(new Error('fail'))
+  }, 3000)
 })
-console.log(promise_prototype)
-
-
-const promise5 = new Promise((resolve) => {
-  resolve('promise5')
-})
-const promise6 = Promise.resolve(promise5).then(v => {
-  console.log(v)
+const p2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve(p1)
+  }, 1000)
 })
 
-console.log(promise6 === promise5)  // false
+p2.then(result => {console.log(result)})
+.catch(error => {console.log(error)})
 
 
-console.log('global start')
-setTimeout(() => {
-  console.log('world')
-},0)
-Promise.resolve('hello').then(v => {
-  console.log('hello')
+
+// -------------- resolve 返回一个 promise ---------------
+const p3 = new Promise(resolve => {
+  setTimeout(() => {
+    resolve('p3')
+  },3000)
 })
-console.log('global end')
+const p4 = new Promise(resolve => {
+  setTimeout(() => {
+    resolve(p3)//p3是一个promise, p4的状态无效了
+  },1000)
+})
+
+p4.then(value => {
+  console.log('p4', value)  // p3
+})
+p3.then(value => {
+  console.log('p3', value)
+})
+
+
+// ----------- ------------
+const p5 = new Promise(resolve => {
+  setTimeout(() => {
+    resolve('p5')
+  },1000)
+})
+const p6 = new Promise(resolve => {
+  setTimeout(() => {
+    resolve(p5)
+  },3000)
+})
+p6.then(value => {
+  console.log('p6', value)  //p5
+})
