44f9efdceeae81dafb55baa9631436024ec894ea
oskarmast <oskarmast9436@gmail.com>
2022-06-08 23:01:34 +0800

    算法数组补充

--- algorithm/webpack.config.js
+++ algorithm/webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   mode: 'development',
-  entry: path.resolve(__dirname, 'src/sort/lack.ts'),
+  entry: path.resolve(__dirname, 'src/array/stack.ts'),
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
