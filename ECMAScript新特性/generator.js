93f00fa4bc7e70c99d6540d943e6fd024d8148dc
oskarmast <oskarmast9436@gmail.com>
2022-07-22 22:08:25 +0800

    generator和bom

--- ECMAScript新特性/generator.js
+++ ECMAScript新特性/generator.js
@@ -8,20 +8,36 @@ console.log(hw.next())
 console.log(hw.next())
 console.log(hw.next())
 
-// ------------------------------- generator
+console.log('function-type:', Object.getPrototypeOf(function* (){}).constructor)  // [Function: GeneratorFunction]
+
+// ---------- 没有return 语句 -------------
+function* player() {
+  yield 'kyrie'
+  yield 'irving'
+}
+const player_next = player()
+console.log(player_next.next())
+console.log(player_next.next())
+console.log(player_next.next())
+/*
+{ value: 'kyrie', done: false }
+{ value: 'irving', done: false }
+{ value: undefined, done: true }
+*/
+
+// ------------------------------- generator ------------
 function *f() {
   return '你好'
   return '世界'
 }
 console.log(f().next())
 
-// ---------------------遍历器生成函数
+// ---------------------遍历器生成函数 -----------------
 const obj = {
   [Symbol.iterator]: function* () {
     yield 1
     yield 2
     yield 3
-    yield 4
   }
 }
 console.log([...obj]) // 1 2 3 4
@@ -105,7 +121,7 @@ function * baz() {
   return 5
 }
 for(let item of baz()){
-  console.log('item---:', item)
+  console.log('item---:', item) // 1 2 3 4
 }
 
 // ------------------------ 给对象部署iterator接口
@@ -124,3 +140,119 @@ const my_object = {
 for(let a of my_object){
   console.log('a:', a)
 }
+
+
+// ------------ Generator.prototype.return --------------
+function* singer() {
+  yield 'hello'
+  yield 'world'
+  yield '你好'
+  yield '生活'
+}
+
+const s = singer()
+s.next()
+console.log(s.return('你好生活'))
+console.log(s.next())
+/*
+{ value: '你好生活', done: true }
+{ value: undefined, done: true }
+*/
+
+// ------------ 调用Generator -----------
+function* m1() {
+  yield 'hello'
+  yield 'world'
+}
+
+function* m2() {
+  console.log(this)
+  yield '你好'
+  yield* m1()
+  yield '生活'
+}
+
+for(const item of m2()) {
+  console.log('item---', item)
+  /*
+  item--- 你好
+  item--- hello
+  item--- world
+  item--- 生活
+  */
+}
+
+// ------------- generator this ------------
+function* p5() {}
+p5.prototype.hello = function(){ return 'hi' }
+const p_next = p5()
+console.log(p_next.hello()) // 'hi'
+
+
+// ---------- 状态机 -------------
+var clock = function* () {
+  while (true) {
+    console.log('Tick!');
+    yield;
+    console.log('Tock!');
+    yield;
+  }
+};
+const c_next = clock()
+console.log(c_next.next())
+console.log(c_next.next())
+console.log(c_next.next())
+console.log(c_next.next())
+
+
+function* Entries(obj) {
+  const keys = Object.keys(obj)
+  for (const key of keys) {
+    yield [key, obj[key]]
+  }
+}
+
+for(const [key, value] of Entries({
+  firstName: 'kyrie',
+  lastName: 'irving',
+  age: 30
+})) {
+  console.log(key, value)
+}
+/*
+firstName kyrie
+lastName irving
+age 30
+*/
+
+
+
+function* player_list() {
+  yield 'lebron'
+  yield 'james'
+}
+console.log([...player_list()])
+console.log(Array.from(player_list()))
+const [firstName, lastName] = player_list()
+console.log(firstName, lastName)
+/*
+[ 'lebron', 'james' ]
+[ 'lebron', 'james' ]
+lebron james
+*/
+
+
+function* calc(x) {
+  const y = 2 * (yield (x+1))
+  const z = yield (y/3)
+  return x + y + z
+}
+const calc_next = calc(5)
+console.log(calc_next.next())
+console.log(calc_next.next(12))
+console.log(calc_next.next(13))
+/*
+{ value: 6, done: false }
+{ value: 8, done: false }
+{ value: 42, done: true }
+*/
