757fb83c4a27d4fee07587be6097b281c07bf677
oskarmast <oskarmast9436@gmail.com>
2022-12-19 22:31:35 +0800

    feat: vue-transition

--- Vue/vue3/src/20-transition.js
+++ Vue/vue3/src/20-transition.js
@@ -2,15 +2,25 @@ const transition_app = createApp({
   setup () {
     const show_visible = ref(true)
     const if_visible = ref(true)
+    const slide_visible = ref(true)
+    const fade_visible = ref(true)
+    const animation_visible = ref(true)
     const tab = ref('singer-list')
     const toggle_component = () => {
       console.log('hello')
       tab.value = tab.value === 'singer-list' ? 'player-list' : 'singer-list';
     }
+    const toggle_slide = () => {
+      slide_visible.value = !slide_visible.value
+    }
     return {
       show_visible,
       if_visible,
+      slide_visible,
+      fade_visible,
+      animation_visible,
       tab,
+      toggle_slide,
       toggle_component
     }
   }
@@ -44,3 +54,91 @@ transition_app.component('singer-list', {
 
 
 transition_app.mount('#transition-app')
+
+
+
+// 监听两个动画
+const transition_animation_app = createApp({
+  setup () {
+    const visible = ref(true)
+    const inner_visible = ref(true)
+    return {
+      visible,
+      inner_visible
+    }
+  }
+})
+
+transition_animation_app.mount('#transition-animation-app')
+
+// js-hook
+const transition_hook_app = createApp({
+  setup () {
+    const visible = ref(true)
+    const before_enter = () => {
+      console.log('before-enter')
+    }
+    const enter = (el, done) => {
+      el.style.opacity = 0
+      done()
+    }
+    const after_enter = (el) => {
+      el.style.opacity = 1
+      el.style.transition = 'opacity .4s;'
+      console.log('after enter')
+    }
+    const enter_cancelled = () => {
+      console.log('enter cancelled')
+    }
+    const before_leave = (el) => {
+      el.style.opacity = 1
+      el.style.transition = 'opacity .4s;'
+      console.log('before leave')
+    }
+    const leave = (el, done) => {
+      console.log('leave')
+      done()
+    }
+    const after_leave = (el) => {
+      el.style.opacity = 0
+      console.log('after leave')
+    }
+    const leave_cancelled = () => {
+      console.log('leave cancelled')
+    }
+    return {
+      visible,
+      before_enter,
+      enter,
+      after_enter,
+      enter_cancelled,
+      before_leave,
+      leave,
+      after_leave,
+      leave_cancelled
+    }
+  }
+})
+transition_hook_app.mount('#transition-hook-app')
+
+
+
+// 可复用的transition
+const transition_slot_app = createApp({
+  setup () {
+    const visible_1 = ref(true)
+    const visible_2 = ref(true)
+    return {
+      visible_1,
+      visible_2
+    }
+  }
+})
+
+transition_slot_app.component('transition-component', {
+  template: `<transition name='my-transition' appear>
+    <slot></slot>
+  </transition>`
+})
+
+transition_slot_app.mount('#transition-slot-app')
