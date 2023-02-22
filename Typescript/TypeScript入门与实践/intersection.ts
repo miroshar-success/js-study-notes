c16b23eee88e1669a49c33366f200b181f36d60e
oskarmast <oskarmast9436@gmail.com>
2023-02-22 21:19:01 +0800

    feat: ts

--- Typescript/TypeScript入门与实践/intersection.ts
+++ Typescript/TypeScript入门与实践/intersection.ts
@@ -75,3 +75,91 @@ const prop_key: PropKeyBothKey = {
   2: false,
   'a': true
 }
+
+// ------------ 索引查询类型 ----------------
+type BookProps = {
+  name: string
+  price: number
+  author: string
+}
+type BookKeyProps = keyof BookProps
+const book_name: BookKeyProps = 'name'
+const book_price: BookKeyProps = 'price'
+
+interface T {
+  [key: string]: boolean
+}
+type KeyofType = keyof T  // string | number
+
+interface NumberKeyT {
+  [key: number]: string
+}
+type NumberKey = keyof NumberKeyT // number
+
+const symbol_key: unique symbol = Symbol()
+type SymbolKeyT = {
+  // [key: symbol]: number
+  [symbol_key]: boolean
+}
+type SymbolKey = keyof SymbolKeyT   // symbol
+
+// ------------- 联合类型使用keyof ------------
+type TypeA = { a: string; b: string }
+type TypeB = { a: boolean; z: number }
+type TypeAB = keyof (TypeA | TypeB)     // 'a'
+
+// ------------- 索引访问类型 ----------------
+type TypeC = { a: boolean; b: string }
+type Ca = 'a'
+type Cb = 'b'
+type TypeCA = TypeC[Ca]
+type TypeCB = TypeC[Cb]
+
+const symbol_string: unique symbol = Symbol()
+const ROLE = 'user/role'
+const enum E {
+  A = 10
+}
+type ComplexType = {
+  0: string
+  x: boolean
+  [E.A]: number
+  [symbol_string]: bigint
+}
+
+type TypeOfNumberLikeName = ComplexType[0]
+type TypeOfStringLikeName = ComplexType['x']
+type TypeOfEnumName = ComplexType[E.A]
+type TypeofSymbolName = ComplexType[typeof symbol_string]
+
+type ComplexTypeKey = 0 | 'x'
+type ValueType = ComplexType[ComplexTypeKey]  // string | boolean
+
+const create_select_options = <T, K extends keyof T, V extends keyof T>(list: T[] = [], label: K, value: V) => {
+  const temp: { label: string; value: number }[] = []
+  list.forEach(item => {
+    temp.push({
+      label: item[label] as unknown as string,
+      value: Number(item[value])
+    })
+  })
+  return temp
+}
+
+// -------------- 索引类型的应用 ------------
+const get_object_property = <T, K extends keyof T>(obj: T, key: K): T[K] => {
+  return obj[key]
+}
+type WatchProps = {
+  name: string
+  price: number
+  color: string
+}
+const watch_props: WatchProps = {
+  name: 'hello',
+  price: 1200,
+  color: 'black'
+}
+const watch_price = get_object_property<WatchProps, 'price'>(watch_props, 'price')
+const watch_name = get_object_property(watch_props, 'name')
+const watch_color = get_object_property(watch_props, 'color')
