c674ede82d9a4e2322a0d49c80029687e6241090
oskarmast <oskarmast9436@gmail.com>
2023-06-01 21:36:02 +0800

    feat:react

--- React/Basic/src/friend-status.jsx
+++ React/Basic/src/friend-status.jsx
@@ -1,6 +1,9 @@
 class ClassFriendStatus extends React.Component {
   constructor(props) {
     super(props)
+    this.state = {
+      isOnline: false
+    }
   }
   componentDidMount() {
     console.log(`开始监听 ${this.props.name} 状态`)
@@ -8,8 +11,13 @@ class ClassFriendStatus extends React.Component {
   componentWillUnmount() {
     console.log(`取消监听 ${this.props.name} 状态`)
   }
+  componentDidUpdate(prevProps) {
+    console.log(`取消监听 ${prevProps.name} 状态`)
+    console.log(`开始监听 ${this.props.name} 状态`)
+  }
   render() {
-    const {name, isOnline} = this.props
+    const { name, id } = this.props
+    const { isOnline } = this.state
     return (
       <li>
         <span>{name} </span>
@@ -21,16 +29,36 @@ class ClassFriendStatus extends React.Component {
   }
 }
 
+const FunctionFriendStatus = (props) => {
+  useEffect(() => {
+    console.log(`开始监听 ${props.name} 状态 ------ function`)
+    return () => {
+      console.log(`取消监听 ${props.name} 状态 ----- function`)
+    }
+  })
+}
+
+const friendList = [
+  {id: 1, isOnline: true, name: 'kyrie' },
+  {id: 2, isOnline: false, name: 'durant' },
+  {id: 3, isOnline: true, name: 'james' }
+]
+
 const App = () => {
-  const [friendList, setFriendList] = useState([
-    {id: 1, isOnline: true, name: 'kyrie' },
-    {id: 2, isOnline: false, name: 'durant' },
-    {id: 3, isOnline: true, name: 'james' }
-  ])
+  const [friend, setFriend] = useState(0)
+  const handleToggleFriend = () => {
+    if (friend === friendList.length - 1) {
+      setFriend(0)
+    } else {
+      setFriend(friend + 1)
+    }
+  }
   return (
-    <ul>
-      { friendList.map(item => <ClassFriendStatus {...item} key={item.id}/>)}
-    </ul>
+    <>
+      <button onClick={handleToggleFriend}>切换好友</button>
+      <ClassFriendStatus {...friendList[friend]}/>
+      <FunctionFriendStatus {...friendList[friend]}/>
+    </>
   )
 }
 
