46bc5243b71162ac56eeac6f61fccf328cf5ac32
oskarmast <oskarmast9436@gmail.com>
2023-06-15 22:45:42 +0800

    feat:js

--- js-Interview/js/flat.js
+++ js-Interview/js/flat.js
@@ -1,3 +1,4 @@
+// -------- 拉平数组 ---------
 const flat = (array) => {
   const is_flat = array.some(item => Array.isArray(item))
   if (!is_flat) return [...array]
@@ -5,8 +6,8 @@ const flat = (array) => {
   return flat(a)
 }
 
-console.log(flat([1, [2, 3, [4, 5], 6], 7]))
-console.log(flat([1, 2, 3, [9, [8,[8,[5,7]]]]]))
+console.log(flat([1, [2, 3, [4, 5], 6], 7]))  // [1,2,3,4,5,6,7]
+console.log(flat([1, 2, 3, [9, [8,[8,[5,7]]]]]))  //  [1, 2, 3, 9, 8, 8, 5, 7 ]
 
 const flat_number = (array, d = 1) => {
   if (d <= 0) return array.slice()
@@ -21,3 +22,47 @@ const flat_number = (array, d = 1) => {
   return t
 }
 console.log(flat_number([[[1, 2], 3], 4], 1))
+
+// --------------- 只拉平一层 ---------------
+const flat_array = (list = []) => {
+  let temp = []
+  list.forEach(item => {
+    if (Array.isArray(item)) {
+      temp = temp.concat(item)
+    } else {
+      temp.push(item)
+    }
+  })
+  return temp
+}
+console.log(flat_array([1, [2, 3, [4, 5], 6], 7])) // [ 1, 2, 3, [ 4, 5 ], 6, 7 ]
+console.log(flat_array([1, 2, 3, [4, 5], 6]))   // [ 1, 2, 3, 4, 5, 6 ]
+
+
+// ------------ 彻底拉平 换种方式 ------------------
+const flat_item = (array = []) => {
+  // console.log('array:', array)
+  let temp = []
+  for (const item of array) {
+    // console.log('item:', item)
+    if (Array.isArray(item)) {
+      temp.push(...flat_item(item))
+      const res = flat_item(item)
+      console.log('res', res)
+    } else {
+      temp.push(item)
+    }
+  }
+  return temp
+}
+// console.log(flat_item([1, [2, 3, [4, 5], 6], 7]))  // [1,2,3,4,5,6,7]
+// console.log(flat_item([1, 2, 3, [9, [8,[8,[5,7]]], 10]]))  //  [1, 2, 3, 9, 8, 8, 5, 7 ]
+console.log(flat_item([1, [2, 3], [5, [6, [7], 8], 9], 4]))
+
+// ------- 每次拉平一层 ----------
+let a1 = [1, 2, 3, [9, [8,[8,[5,7]]]]]
+for(let i = 0; i < 4; i++) {
+  // a1 = Array.prototype.concat.apply([], a1)
+  a1 = [].concat(...a1)
+}
+console.log('a1:', a1)
---
+++ js-Interview/js/home-optimization-performance.js
@@ -0,0 +1,11 @@
+/**
+ * 首屏优化
+ * 1. 路由懒加载
+ * 2. 服务端渲染 (H5页面, SSR是性能优化的终极方案)
+  3. 分页显示
+  4. 图片懒加载 lazyLoad 提前设置尺寸, 尽量只重绘,不重排 
+  5. 性能优化也可以配合用户体验
+*/
+
+// lighthouse
+// lighthouse url --view --preset=desktop
