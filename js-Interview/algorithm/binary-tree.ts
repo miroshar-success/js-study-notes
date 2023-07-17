806e3cf7406a2a1c7725cab8621f11e13490e522
oskarmast <oskarmast9436@gmail.com>
2023-07-17 22:33:44 +0800

    feat: js算法

---
+++ js-Interview/algorithm/binary-tree.ts
@@ -0,0 +1,80 @@
+type BinaryTreeProps = {
+  value: number;
+  left?: BinaryTreeProps
+  right?: BinaryTreeProps
+}
+
+const binary_tree: BinaryTreeProps = {
+  value: 1,
+  left: {
+    value: 2,
+    left: {
+      value: 4,
+      left: {
+        value: 100,
+        left: {
+          value: 200,
+          left: {
+            value: 500,
+            left: {
+              value: 600
+            }
+          }
+        }
+      },
+      right: {
+        value: 300
+      }
+    },
+    right: {
+      value: 7,
+      left: {
+        value: 6,
+        left: {
+          value: 11,
+          left: {
+            value: 12,
+            left: {
+              value: 13
+            }
+          }
+        }
+      },
+      right: {
+        value: 10
+      }
+    }
+  },
+  right: {
+    value: 3,
+    left: {
+      value: 5
+    },
+    right: {
+      value: 8
+    }
+  }
+}
+const preorder_traversal = (node: BinaryTreeProps | undefined) => {
+  if (!node) return
+  console.log(node)
+  preorder_traversal(node.left)
+  preorder_traversal(node.right)
+}
+preorder_traversal(binary_tree)
+// 调用栈
+/**
+ * [
+ *  {value: 2, left: {value: 4}, right: {value: 7, left: {value: 6}, right: {value: 10}}}
+    {value: 4}
+* ]
+ * 
+*/
+console.log('----------------- 中序遍历 ----------------')
+const midorder_traversal = (node: BinaryTreeProps | undefined) => {
+  if (!node) return
+  midorder_traversal(node.left)
+  console.log(node)
+  midorder_traversal(node.right)
+}
+// midorder_traversal(binary_tree)
