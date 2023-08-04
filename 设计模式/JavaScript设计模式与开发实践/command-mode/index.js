5c2216d59808ac5cc0db5948b749ed0a7d1072fd
oskarmast <oskarmast9436@gmail.com>
2023-08-04 10:24:58 +0800

    feat: 设计模式-组合模式

--- 设计模式/JavaScript设计模式与开发实践/command-mode/index.js
+++ 设计模式/JavaScript设计模式与开发实践/command-mode/index.js
@@ -29,3 +29,89 @@ MenuBarCommand.prototype.execute = function() {
 setCommand($('.button-1'), new MenuBarCommand(add_menu))
 setCommand($('.button-2'), new MenuBarCommand(refresh_menu))
 setCommand($('.button-3'), new MenuBarCommand(del_menu))
+
+// ------------------ 支持撤销 -----------------------
+;(function () {
+  const commands = {
+    right: function () {
+      console.log('向右移动')
+    },
+    left: function() {
+      console.log('向左移动')
+    },
+    jump: function() {
+      console.log('跳跃')
+    },
+    crouch: function() {
+      console.log('蹲下')
+    }
+  }
+  const makeCommand = function(receiver, state) {
+    return function () {
+      return receiver[state]()
+    }
+  }
+  const keyCodeCommandMap = {
+    38: 'jump',
+    40: 'crouch',
+    37: 'left',
+    39: 'right'
+  }
+  const commandStack = []
+  document.onkeyup = function(e) {
+    const keyCode = e.keyCode
+    if (!keyCodeCommandMap[keyCode]) return false
+    const command = makeCommand(commands, keyCodeCommandMap[keyCode])
+    if (command) {
+      command()
+      commandStack.push(command) 
+    }
+  }
+
+  const btn = document.querySelector('.reset-btn')
+  btn.addEventListener('click', () => {
+    let command = commandStack.pop()
+    while (command) {
+      command()
+      command = commandStack.pop()
+    }
+  }, false)
+})();
+
+// ---------------------- 宏命令 ----------------------------
+;(function () {
+  const closeDoorCommand = {
+    execute: function() {
+      console.log('关门')
+    }
+  }
+  const openComputerCommand = {
+    execute: function() {
+      console.log('打开电脑')
+    }
+  }
+  const openQQCommand = {
+    execute: function() {
+      console.log('登录QQ')
+    }
+  }
+  const MacroCommand = function () {
+    return {
+      commandList: [],
+      add: function (command) {
+        this.commandList.push(command)
+      },
+      execute: function () {
+        for (let i = 0, length = this.commandList.length; i < length; i++) {
+          const command = this.commandList[i]
+          command.execute()
+        }
+      }
+    }
+  }
+  const macro_command = MacroCommand()
+  macro_command.add(closeDoorCommand)
+  macro_command.add(openComputerCommand)
+  macro_command.add(openQQCommand)
+  macro_command.execute()
+})()
