593a14d056abd0727ca1e20d9dea5fdf030f4af0
oskarmast <oskarmast9436@gmail.com>
2023-08-27 22:23:09 +0800

    feat: nest

--- Node/readme.md
+++ Node/readme.md
@@ -1,31 +1,33 @@
 # Node.js
 
-1. APM(应用监控 Application Performance Management) 包括Agent(上报数据)/Monitor(收集数据)/Dashboard(展示数据)
-  概念 / 搭建 / AliNode
+1. APM(应用监控 Application Performance Management) 包括 Agent(上报数据)/Monitor(收集数据)/Dashboard(展示数据)
+   概念 / 搭建 / AliNode
 2. 高可用
-  负载均衡(服务负载均衡和RPC负载均衡) / 灰度发布 / 优雅退出
+   负载均衡(服务负载均衡和 RPC 负载均衡) / 灰度发布 / 优雅退出
 3. 日志
-  日志原理 / 日志分析 / 日志监控 / 日志收集与处理
-  ELK架构 Sentry日志收集平台
+   日志原理 / 日志分析 / 日志监控 / 日志收集与处理
+   ELK 架构 Sentry 日志收集平台
 4. 稳定性
-  安全风险 / 限流 / 错误处理
+   安全风险 / 限流 / 错误处理
 5. 测试
-  单元测试 / 代码质量 / 性能测试
-  Code Smell 以及代码质量检查工具 SonarQube
+   单元测试 / 代码质量 / 性能测试
+   Code Smell 以及代码质量检查工具 SonarQube
 6. 内存泄露
-  内存回收原理 / 内存问题排查实战
+   内存回收原理 / 内存问题排查实战
 7. 灰度发布机制与健康检查
 8. 安全风险防范
 9. 异常处理
 
 ## nvm
 
-  使用git下载:
+使用 git 下载:
+
 ```js
 cd ~/ git clone https://github.com/nvm-sh/nvm.git .nvm
 . ./nvm.sh
 ```
-[nvm下载](https://github.com/nvm-sh/nvm)
+
+[nvm 下载](https://github.com/nvm-sh/nvm)
 
 ## 镜像管理
 
@@ -39,36 +41,35 @@ npm config get registry (获取当前的镜像源)
 
 ## USE Method
 
-  Utilization: 利用率 (以资源一个时间段内被使用的百分比来表示)
-  Saturation  饱和度  (某个资源排队的数量)
-  Errors      误差    (出现异常的数量)
+Utilization: 利用率 (以资源一个时间段内被使用的百分比来表示)
+Saturation 饱和度 (某个资源排队的数量)
+Errors 误差 (出现异常的数量)
 
-  计算密集型: 计算/逻辑判断量非常大并且集中的类型,因为主要占用CPU资源所以又叫CPU密集型,当计算任务数等于CPU核心数的时候,是CPU运行效率最高的时候。
+计算密集型: 计算/逻辑判断量非常大并且集中的类型,因为主要占用 CPU 资源所以又叫 CPU 密集型,当计算任务数等于 CPU 核心数的时候,是 CPU 运行效率最高的时候。
 
-  I/O 密集型: 当磁盘的读取数据和输出数据非常大的类型。由于I/O操作的运行时间远远大于CPU,内存运行时间,所以任务的大部分时间都是在
-  等待IO操作完成,I/O的特点是CPU消耗小。
+I/O 密集型: 当磁盘的读取数据和输出数据非常大的类型。由于 I/O 操作的运行时间远远大于 CPU,内存运行时间,所以任务的大部分时间都是在
+等待 IO 操作完成,I/O 的特点是 CPU 消耗小。
 
-  rss: Resident Set Size: 为进程执行分配的总内存
-  heapTotal:    V8分配的堆总大小
-  heapUsed:     V8执行期间实际使用的内存
+rss: Resident Set Size: 为进程执行分配的总内存
+heapTotal: V8 分配的堆总大小
+heapUsed: V8 执行期间实际使用的内存
 
+autocannon (压力测试)
+clinic
 
-  autocannon (压力测试)
-  clinic
-
-  https://gitee.com/node-apm/nodejs-memory-leak/blob/master/index.js
+https://gitee.com/node-apm/nodejs-memory-leak/blob/master/index.js
 
 ## QPS(Query Per Second)
 
-  用来衡量服务的性能。QPS指每秒钟能执行的query。QPS的数值越高,server能处理的request越多。
+用来衡量服务的性能。QPS 指每秒钟能执行的 query。QPS 的数值越高,server 能处理的 request 越多。
 
-  峰值QPS: 每天80%的访问集中在20%的时间里, 这20%的时间叫做峰值时间。
+峰值 QPS: 每天 80%的访问集中在 20%的时间里, 这 20%的时间叫做峰值时间。
 
-  RT: (Response-time) 响应时间: 执行一个请求从开始到最后收到响应数据所花费的总体时间, 即从客户端发起请求到收到服务器响应结果的时间。
+RT: (Response-time) 响应时间: 执行一个请求从开始到最后收到响应数据所花费的总体时间, 即从客户端发起请求到收到服务器响应结果的时间。
 
-  并发数指系统同时能处理的请求数量, 反应了系统的负载能力。
+并发数指系统同时能处理的请求数量, 反应了系统的负载能力。
 
-  Graphite / Grafana 监控Node.js应用
+Graphite / Grafana 监控 Node.js 应用
 
 # Nestjs
 
@@ -78,50 +79,106 @@ npm config get registry (获取当前的镜像源)
 npm i -g @nestjs/cli
 nest new project-name
 ```
-  使用--strict 标识创建一个TypeScript的项目。
+
+使用--strict 标识创建一个 TypeScript 的项目。
 
 ```js
 npm run start
 ```
-  启动服务, 默认监听在3000端口, 如果需要监听文件的变化, 使用下面的命令:
+
+启动服务, 默认监听在 3000 端口, 如果需要监听文件的变化, 使用下面的命令:
+
 ```js
 npm run start:dev
 ```
-  This command will watch your files, automatically recompiling and reloading the server.
 
-  To create a Nest application instance, we use the core **NestFactory** class. *NestFactory* exposes a few static
-  methods that allow creating an application instance.
+This command will watch your files, automatically recompiling and reloading the server.
+
+To create a Nest application instance, we use the core **NestFactory** class. _NestFactory_ exposes a few static
+methods that allow creating an application instance.
+
+```js
+app.setGlobalPrefix("/api/v1"); // 设置所有路径的前缀
+```
 
 ## Controllers
 
-  Controllers are responsible for handling incoming **requests** and returning **responses** to the client.
+Controllers are responsible for handling incoming **requests** and returning **responses** to the client.
+
 ```js
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get } from "@nestjs/common";
 @Controller() // 可指定一个路由前缀（specify an optional route path prefix of cats)
 // easily group set of related routes.
 export class CatsController {
   @Get() // 此处可以添加路径信息。(tha path includes both the optional controller path prefix and any path
   // string declared in the request method decorator)
-  findAll (): string {
-    return 'all cats'
+  findAll(): string {
+    return "all cats";
   }
 }
 ```
-  when a request handler returns a JavaScript object or array, it will automatically be serialized to JSON.When it
-  returns a JavaScript primitive type, however, Nest will send just the value without attempting to serialize it.
+
+when a request handler returns a JavaScript object or array, it will automatically be serialized to JSON.When it
+returns a JavaScript primitive type, however, Nest will send just the value without attempting to serialize it.
+
+### Utils
+
+使用内置的命令行工具快速创建文件
+
+```js
+nest g controller user --no-spec
+nest g service user --no-spec
+
+// ...
+nest g decorator <name>
+nest g interface <name>
+// 可以通过 nest --help 命令查看支持创建的文件类型
+```
 
 ### Request Object
 
-  获取请求对象相关信息(Handlers ofen need access to the client **request** details).
-```js
-import { Req } from '@nestjs/common'
-import { Request } from 'express'
+获取请求对象相关信息(Handlers ofen need access to the client **request** details).
 
-@Controller('cats')
+```js
+import { Req } from "@nestjs/common";
+import { Request } from "express";
+
+@Controller("cats")
 export class CatsController {
   @Get()
   findAll(@Req() request: Request) {
-    console.log(request) // 请求相关对象
+    console.log(request); // 请求相关对象
+  }
+  @Get("ab*cd") // 路由通配符只有 express 支持
+  findAll() {
+    return "this route uses a wildcard";
+  }
+}
+```
+
+## Dymanic Params
+
+```js
+export class SingerService {
+  @Get(":id")
+  findOne(@Param() params: any): string {
+    console.log(params.id);
+  }
+  /**
+   * @Param() is used to decorate a method parameter, and makes the route parameters available properties of that
+   * decorated method parameter inside the body of the method.
+   */
+}
+```
+
+## Async / Await
+
+```js
+@Controller()
+export class PlayerController {
+  async findAll() {
+    const res = await axios.post("http://xxx.com/api/v1/players");
+    return res;
   }
 }
 ```
--- Typescript/function.ts
+++ Typescript/function.ts
@@ -2,98 +2,93 @@
  * we can use a type alias to name a function type
  *
  */
-type GreetFunction = (a: string) => void
+type GreetFunction = (a: string) => void;
 const greeter: GreetFunction = (message) => {
-  console.log(message)
-}
-greeter('hello')
-
+  console.log(message);
+};
+greeter("hello");
 
 // --------- Call signatures ---------
 type DescribableFunction = {
-  description: string;  // 函数的属性
-  (title:string) : string // 函数参数
-}
+  description: string; // 函数的属性
+  (title: string): string; // 函数参数
+};
 const description: DescribableFunction = (message) => {
-  return message
-}
-description.description = '你好'
-console.log(description('Hello') + '-' + description.description) // Hello-你好
-
+  return message;
+};
+description.description = "你好";
+console.log(description("Hello") + "-" + description.description); // Hello-你好
 
 // -------- Generic Functions --------
-function firstElement<Type>(array:Type[]): Type {
-  return array[0]
+function firstElement<Type>(array: Type[]): Type {
+  return array[0];
 }
-console.log(firstElement<string>(['hello', 'world'])) // hello
-console.log(firstElement<number>([1,2,3]))            // 1
-console.log(firstElement<boolean>([false, true]))     // false
-
+console.log(firstElement<string>(["hello", "world"])); // hello
+console.log(firstElement<number>([1, 2, 3])); // 1
+console.log(firstElement<boolean>([false, true])); // false
 
 function map<I, U>(array: I[], func: (arg: I) => U): U[] {
-  return array.map(func)
+  return array.map(func);
 }
-map(['1', '2', '3'], (n) => Number.parseInt(n, 10))
-
-
+map(["1", "2", "3"], (n) => Number.parseInt(n, 10));
 
 // ----------- 泛型约束 ----------
-function longest<Type extends {length: number}>(a: Type, b: Type) {
-  if(a.length >= b.length) {
-    return a
+function longest<Type extends { length: number }>(a: Type, b: Type) {
+  if (a.length >= b.length) {
+    return a;
   } else {
-    return b
+    return b;
   }
 }
-console.log(longest([1,2], [3,4,5,6,7]))  // [ 3, 4, 5, 6, 7 ]
-console.log(longest([1,2,3,4,5], [1, 2])) // [ 1, 2, 3, 4, 5 ]
+console.log(longest([1, 2], [3, 4, 5, 6, 7])); // [ 3, 4, 5, 6, 7 ]
+console.log(longest([1, 2, 3, 4, 5], [1, 2])); // [ 1, 2, 3, 4, 5 ]
 
 // --------- constrained values -----------
-function minimumLength<Type extends {length: number}>(obj: Type, minimum: number): Type {
-  if(obj.length >= minimum) {
-    return obj
+function minimumLength<Type extends { length: number }>(
+  obj: Type,
+  minimum: number
+): Type {
+  if (obj.length >= minimum) {
+    return obj;
   } else {
     return {
       ...obj,
-      length: minimum
-    }
+      length: minimum,
+    };
   }
 }
 
 // ------- Specifying Type Arguments --------
-function combine<Type>(arr1: Type[], arr2:Type[]): Type [] {
-  return [...arr1, ...arr2]
+function combine<Type>(arr1: Type[], arr2: Type[]): Type[] {
+  return [...arr1, ...arr2];
 }
 
-function filter<T>(array: T[], func:(arg: T) => boolean): T[] {
-  return array.filter(func)
+function filter<T>(array: T[], func: (arg: T) => boolean): T[] {
+  return array.filter(func);
 }
-console.log(filter<number>([1,2,3,4,5], (n) => n > 5))
-
-
+console.log(filter<number>([1, 2, 3, 4, 5], (n) => n > 5));
 
 // ------- Optional Parameters ----------
-function calc(x?: number): void{
-  console.log(x)
+function calc(x?: number): void {
+  console.log(x);
 }
 
-calc(1)         // 1
-calc(undefined) // undefined
-calc()          // undefined
-
+calc(1); // 1
+calc(undefined); // undefined
+calc(); // undefined
 
 // --------- optional parameters in callbacks ---------
-function foreach(array: any[], callback:(arg: any, index: number) => void) {
-  for(let i = 0, length = array.length; i < length; i++) {
-    callback(array[i], i)
+function foreach(array: any[], callback: (arg: any, index: number) => void) {
+  for (let i = 0, length = array.length; i < length; i++) {
+    callback(array[i], i);
   }
 }
-foreach([1,2,3,4,5],(item) => {
-  console.log(item) // 1 2 3 4 5
-})
-foreach(['kyrie', 'wade', 'lebron', 'durant'], player => {
-  console.log(player) // kyrie wade lebron durant
-})
+foreach([1, 2, 3, 4, 5], (item) => {
+  console.log(item); // 1 2 3 4 5
+});
+foreach(["kyrie", "wade", "lebron", "durant"], (player) => {
+  console.log(player); // kyrie wade lebron durant
+});
 /*
 In JavaScript, if you call a function with more arguments than there are parameters, the extra arguments are
 simply ignored, TypeScript the same way. Functions with fewer parameters can always take the place of functions
@@ -103,78 +98,78 @@ with more parameters
 /*
 Some JavaScript functions can be called in a variety of argument counts and types.
 */
-function date(timestamp: number): Date
+function date(timestamp: number): Date;
 function date(m: number, d: number, y: number): Date;
-function date(timestamp: number, d?:number, y?:number): Date {
-  if(d !== undefined && y !== undefined) {
-    return new Date(timestamp, d, y)
+function date(timestamp: number, d?: number, y?: number): Date {
+  if (d !== undefined && y !== undefined) {
+    return new Date(timestamp, d, y);
   }
-  return new Date(timestamp)
+  return new Date(timestamp);
 }
-console.log(date(2022, 5, 17))
-console.log(date(1652791785619))
+console.log(date(2022, 5, 17));
+console.log(date(1652791785619));
 
 /*
 The signature of the implementation is not visible from the outside, when writing an overloaded function,
 you should always have two or more signatures above the implementation of the function.
 */
-
-function get_length(s: string): number
-function get_length(array: number[]): number
-function get_length(s:string | number[]): number {
-  return s.length
+// 函数重载不能使用 箭头函数
+function get_length(s: string): number;
+function get_length(array: number[]): number;
+function get_length(s: string | number[]): number {
+  return s.length;
 }
-console.log(get_length([1,2,3,4,5]))    // 5
-console.log(get_length('hello world'))  // 11
+console.log(get_length([1, 2, 3, 4, 5])); // 5
+console.log(get_length("hello world")); // 11
 
 // ------ 函数重载 ------
-type MessageType = 'image' | 'audio' | 'text'
+type MessageType = "image" | "audio" | "text";
 type Message = {
-  title: string
-  id: number
-  type: MessageType
-}
+  title: string;
+  id: number;
+  type: MessageType;
+};
 const messages: Message[] = [
   {
     id: 1,
-    title: 'Vue.js',
-    type: 'text'
+    title: "Vue.js",
+    type: "text",
   },
   {
     id: 2,
-    title: 'React',
-    type: 'image'
+    title: "React",
+    type: "image",
   },
   {
     id: 3,
-    title: 'Redux',
-    type: 'audio'
+    title: "Redux",
+    type: "audio",
   },
   {
     id: 4,
-    title: 'Vuex',
-    type: 'text'
+    title: "Vuex",
+    type: "text",
   },
   {
     id: 5,
-    title: 'React-Redux',
-    type: 'image'
-  }
-]
-function get_message_list(id: number): Message []
-function get_message_list(type: MessageType): Message[]
+    title: "React-Redux",
+    type: "image",
+  },
+];
+function get_message_list(id: number): Message[];
+function get_message_list(type: MessageType): Message[];
 
-function get_message_list(type: number | MessageType):Message[] {
-  if(typeof type === 'number') {
-    const find = messages.find(m => m.id === type)
-    if(find) return [find]
-    return []
-  }else{
-    return messages.filter(m => m.type === type)
+function get_message_list(type: number | MessageType): Message[] {
+  if (typeof type === "number") {
+    const find = messages.find((m) => m.id === type);
+    if (find) return [find];
+    return [];
+  } else {
+    return messages.filter((m) => m.type === type);
   }
 }
-console.log(get_message_list(4))  // [ { id: 4, title: 'Vuex', type: 'text' } ]
-console.log(get_message_list('image'))
+console.log(get_message_list(4)); // [ { id: 4, title: 'Vuex', type: 'text' } ]
+console.log(get_message_list("image"));
 /*
 [
   { id: 2, title: 'React', type: 'image' },
@@ -185,43 +180,39 @@ console.log(get_message_list('image'))
 // ------------- Declaring this in a function ------------
 const user = {
   age: 30,
-  firstName: 'kyrie',
-  lastName: 'irving',
+  firstName: "kyrie",
+  lastName: "irving",
   fullName() {
-    return this.firstName + this.fullName
-  }
-}
-
+    return this.firstName + this.fullName;
+  },
+};
 
 // ------ rest parameters and arguments ------
 function multiply(n: number, ...m: number[]) {
-  return m.map(x => x * n)
+  return m.map((x) => x * n);
 }
-console.log(multiply(2,2,3,4,5,6,7,8))
+console.log(multiply(2, 2, 3, 4, 5, 6, 7, 8));
 /*
 [4,  6,  8, 10, 12, 14, 16]
 */
 
-const args = [8, 5] as const
-console.log(Math.atan2(...args))
-
+const args = [8, 5] as const;
+console.log(Math.atan2(...args));
 
 // ---------- parameter destructuring ----------
-function increment ({a, b, c}: {a:number; b:number; c:number}): number {
-  return a + b + c
+function increment({ a, b, c }: { a: number; b: number; c: number }): number {
+  return a + b + c;
 }
-increment({a: 10, b: 20, c: 30})
+increment({ a: 10, b: 20, c: 30 });
 
+type voidFunc = () => void;
+const hello1: voidFunc = () => 123;
+const hello2: voidFunc = () => true;
+const hello3: voidFunc = () => false;
+const hello4: voidFunc = () => "123";
+console.log(hello1(), hello2(), hello3(), hello4()); // 123  true  false '123'
 
-type voidFunc = () => void
-const hello1:voidFunc = () => 123
-const hello2:voidFunc = () => true
-const hello3:voidFunc = () => false
-const hello4:voidFunc = () => '123'
-console.log(hello1(), hello2(), hello3(), hello4()) // 123  true  false '123'
-
-
-const source = [1, 2, 3, 4, 5]
-const dest = [0]
-source.forEach(el => dest.push(el))
-console.log(dest) // [ 0, 1, 2, 3, 4, 5 ]
+const source = [1, 2, 3, 4, 5];
+const dest = [0];
+source.forEach((el) => dest.push(el));
+console.log(dest); // [ 0, 1, 2, 3, 4, 5 ]
